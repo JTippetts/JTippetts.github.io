fcecf76f821a445a72ed2ec2f928df251d31e2c5
JTippetts <vertexnormal@gmail.com>
2021-06-19 05:41:47 -0600

    Update GoldRush.

--- Resources.js
+++ Resources.js
@@ -341,7 +341,7 @@
     }
   
    }
-   loadPackage({"files": [{"filename": "/Data.pak", "start": 0, "end": 10120483, "audio": 0}, {"filename": "/CoreData.pak", "start": 10120483, "end": 10276034, "audio": 0}], "remote_package_size": 10276034, "package_uuid": "7dad1ae4-0de9-499f-b193-01897ed9a5e6"});
+   loadPackage({"files": [{"filename": "/Data.pak", "start": 0, "end": 10120483, "audio": 0}, {"filename": "/CoreData.pak", "start": 10120483, "end": 10276034, "audio": 0}], "remote_package_size": 10276034, "package_uuid": "f520f372-1f02-4710-a1bb-c35ed6cc4948"});
   
   })();
   
